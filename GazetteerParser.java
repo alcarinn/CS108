b6b03db1f3d4c220148a32eeed1a089c45953024
Nicolas <nicolas.dargenlieu@epfl.ch>
2017-03-24 13:46:22 +0100

    Update GazetteerParser.java

--- GazetteerParser.java
+++ GazetteerParser.java
@@ -14,10 +14,24 @@ import java.util.Collections;
 
 import static ch.epfl.alpano.Preconditions.checkArgument;
 
+/**
+ * Represents a file reader returning the summit modeled with the file's informations
+ * @author Deniz Ira (269728) & Nicolas d'Argenlieu (276507)
+ */
+
 public final class GazetteerParser {
     
+    /**
+     * Private constructor making it impossible to instantiate the class
+     */
     public GazetteerParser() {} ;
     
+    /**
+     * Loads an input file with informations about summit, the informations are written following a standard format, one line for each summit.
+     * Gets the informations and puts them into a table. The method returns the table as an unmodifiable Collection.
+     * @param file the input file containing the summit informations
+     * @return (List<Summit>) unmodifiableCollection list with all the summit from the file
+     */
     public static List<Summit> readSummitsFrom(File file) {
         List<Summit> list = new ArrayList<Summit>();
         
@@ -41,7 +55,13 @@ public final class GazetteerParser {
         return Collections.unmodifiableList(list);
         
     }
-    
+   
+    /**
+     * Computes the given angle that is expressed in degrees, minutes and seconds into an angle only in degrees.
+     * @param angle
+     * @return the angle's value in degrees only
+     * @throws IOException if the table representing the angle has more than 3 components (degrees, minutes, seconds)
+     */
     private static double switcher(String[] angle) throws IOException {
         checkArgument(angle.length == 3);
         checkIOException(0<=Integer.parseInt(angle[1]) && Integer.parseInt(angle[1])<60);
@@ -49,7 +69,12 @@ public final class GazetteerParser {
         return Integer.parseInt(angle[0]) + Integer.parseInt(angle[1])/60.0 + Integer.parseInt(angle[2])/3600.0;
     }
     
-    
+    /**
+     * Reads and gets each single information about the summit described in the line passed as input value : name, longitude, latitude, elevation
+     * @param line String obtained from the file containing all the summit's information, it contains the wanted information
+     * @return New Summit instantiated with the informations retrieved from the input String
+     * @throws IOException if at least one argument is not valid
+     */
     private static Summit decode(String line) throws IOException {
         checkIOException(line.charAt(9) == ' ');
         checkIOException(line.charAt(18) == ' ');
@@ -76,6 +101,11 @@ public final class GazetteerParser {
         return new Summit(name, new GeoPoint(Math.toRadians(Long), Math.toRadians(Lat)), elevation);
     }
     
+    /**
+     * Throws an IOException if the boolean check passed as input value is false
+     * @param b result of a boolean check
+     * @throws IOException if the check is not valid
+     */
     private static void checkIOException(boolean b) throws IOException{
         if(!b){
             throw new IOException();
